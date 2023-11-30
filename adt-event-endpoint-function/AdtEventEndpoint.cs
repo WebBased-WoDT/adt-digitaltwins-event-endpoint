abf94459df645b20b999b9663c568a81f50c755d
andrea.giulianelli <andrygiulio@gmail.com>
2023-11-30 14:32:26 +0100

    chore: change function name to broadcast and event type to CloudEvents

--- adt-event-endpoint-function/AdtEventEndpoint.cs
+++ adt-event-endpoint-function/AdtEventEndpoint.cs
@@ -2,14 +2,14 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
-using Azure.Messaging.EventGrid;
+using Azure.Messaging;
 
 namespace AdtEventEndpoint
 {
     public static class AdtEventEndpoint
     {
-        [FunctionName("AdtEventEndpoint")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        [FunctionName("broadcast")]
+        public static void Run([EventGridTrigger] CloudEvent eventGridEvent, ILogger log)
         {
             log.LogInformation(eventGridEvent.Data.ToString());
         }
