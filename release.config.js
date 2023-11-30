52a4feef6851a3c889a4992e2273359c00ec9169
andrea.giulianelli <andrygiulio@gmail.com>
2023-11-30 11:39:22 +0100

    ci: add build and deploy and release workflow

---
+++ release.config.js
@@ -0,0 +1,32 @@
+// Load this configuration that provide all the base for working with conventional commits
+const config = require('semantic-release-preconfigured-conventional-commits')
+
+/*
+ Commands executed during release.
+*/
+const publishCommands = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 2
+git push --force origin \${nextRelease.version} || exit 3
+`
+// Only release on branch main
+const releaseBranches = ["main"]
+
+config.branches = releaseBranches
+
+config.plugins.push(
+    // Custom release commands
+    ["@semantic-release/exec", {
+        "publishCmd": publishCommands,
+    }],
+    // Release also in GitHub
+    ["@semantic-release/github", {
+        "assets": []
+    }],
+    ["@semantic-release/git", {
+        "assets": ["CHANGELOG.md", "package.json"],
+        "message": "chore(release)!: [skip ci] ${nextRelease.version} released"
+    }]
+)
+
+// JS Semantic Release configuration must export the JS configuration object
+module.exports = config
