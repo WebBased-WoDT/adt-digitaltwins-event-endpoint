2e39621584196ca5593cba4c170de74d867d3a6a
andrea.giulianelli <andrygiulio@gmail.com>
2023-11-30 17:18:36 +0100

    build: set dotnet 6 instead of 8

--- adt-event-endpoint-function/ADTEventTrigger.cs
+++ adt-event-endpoint-function/ADTEventTrigger.cs
@@ -1,14 +1,16 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
 using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 using Azure.Messaging;
 
-namespace AdtEventEndpoint
+namespace Unibo.Wodt
 {
-    public static class AdtEventEndpoint
+    public static class ADTEventTrigger
     {
-        [FunctionName("broadcast")]
+        [FunctionName("broadcaster")]
         public static void Run([EventGridTrigger] CloudEvent eventGridEvent, ILogger log)
         {
             log.LogInformation(eventGridEvent.Data.ToString());
