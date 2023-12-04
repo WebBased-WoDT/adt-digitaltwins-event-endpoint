a9de2f73c545b14660ede3a4b20ee04e3db1cde7
andrea.giulianelli <andrygiulio@gmail.com>
2023-12-04 17:33:35 +0100

    refactor: refactor code to handle events

--- adt-event-endpoint-function/ADTEventTrigger.cs
+++ adt-event-endpoint-function/ADTEventTrigger.cs
@@ -1,3 +1,5 @@
+using Azure.Core.Pipeline;
+using Azure.Identity;
 using Azure.Messaging;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -8,11 +10,32 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Azure.DigitalTwins.Core;
+using System;
+using System.Net.Http;
 
 namespace Unibo.Wodt
 {
     public static class ADTEventTrigger
     {
+        /// <summary> Azure Digital Twin's relationships events type. </summary>
+        private const string relationshipEventType = "Microsoft.DigitalTwins.Relationship";
+        /// <summary> Azure Digital Twin's create Digital Twin event type. </summary>
+        private const string createDigitalTwinEventType = "Microsoft.DigitalTwins.Twin.Create";
+        /// <summary> Azure Digital Twin's delete Digital Twin event type. </summary>
+        private const string deleteDigitalTwinEventType = "Microsoft.DigitalTwins.Twin.Delete";
+        /// <summary> Azure Digital Twin's update Digital Twin event type. </summary>
+        private const string updateDigitalTwinEventType = "Microsoft.DigitalTwins.Twin.Update";
+
+        /// <summary> The ID of the interested Digital Twin. </summary>
+        private const string digitalTwinId = "ambulance";
+
+        /// <summary> The Azure Digital Twins client. </summary>
+        private static DigitalTwinsClient digitalTwinsClient = new DigitalTwinsClient(
+                new Uri(Environment.GetEnvironmentVariable("ADT_SERVICE_URL")),
+                new DefaultAzureCredential(),
+                new DigitalTwinsClientOptions{ Transport = new HttpClientTransport(new HttpClient()) });
+
          /// <summary>A HTTP trigger function. It is used by client to be able to connect to SignalR Service.
         /// It uses the SignalRConnectionInfo input binding
         /// to generate and return valid connection information.</summary>
@@ -27,7 +50,7 @@ namespace Unibo.Wodt
         }
 
         /// <summary>This Azure function handle the events from the Event Grid topic 
-        /// to which it is subscribed in order to receive Azure Digital Twins events.</summary>
+        /// to which it is subscribed in order to receive Azure Digital Twins events.
         /// <param name="eventGridEvent">the trigger of the function. It is an event from the Event Grid. The specification used is CloudEvent v1.</param>
         /// <param name="signalRConnection">the output binding to the SignalR connection used to send the events.</param>
         [FunctionName("broadcaster")]
@@ -36,23 +59,45 @@ namespace Unibo.Wodt
             [SignalR(HubName = "dteventendpointhub", ConnectionStringSetting = "AzureSignalRConnectionString")] IAsyncCollector<SignalRMessage> signalRConnection,
             ILogger log)
         {
+            // Log received event
+            log.LogInformation($"Received event: {eventGridEvent.Data}");
+
             // Obtain event data and construct the event object to send via SignalR
             JObject eventToClients = (JObject)JsonConvert.DeserializeObject(eventGridEvent.Data.ToString());
-            // Add metadata to the event object
-            eventToClients.Add("id", eventGridEvent.Subject);
-            eventToClients.Add("eventType", eventGridEvent.Type);
-            eventToClients.Add("eventDateTime", eventGridEvent.Time);
-            log.LogInformation(eventGridEvent.Data.ToString());
 
-            log.LogInformation($"New event:\n {eventToClients.ToString()}");
+            // Filter out event that aren't of the Digital Twin of interest
+            if (getDigitalTwinId(eventGridEvent.Type, eventGridEvent).Equals(digitalTwinId)){
+                // Add metadata to the event object
+                eventToClients.Add("DtId", getDigitalTwinId(eventGridEvent.Type, eventGridEvent));
+                eventToClients.Add("eventType", eventGridEvent.Type);
+                eventToClients.Add("eventDateTime", eventGridEvent.Time);
 
-            // Send the event via SignalR
-            return signalRConnection.AddAsync(
-                new SignalRMessage
-                {
-                    Target = "newMessage",
-                    Arguments = new[] { JsonConvert.SerializeObject(eventToClients) }
-                });
+                // Log event sent via Signal R
+                log.LogInformation($"New event:\n {eventToClients}");
+
+                // Send the event via SignalR
+                return signalRConnection.AddAsync(
+                    new SignalRMessage
+                    {
+                        Target = "digitalTwinUpdate",
+                        Arguments = new[] { JsonConvert.SerializeObject(eventToClients) }
+                    });
+            } else {
+                return Task.CompletedTask;
+            }
+        }
+
+        private static string getDigitalTwinId(string eventType, CloudEvent receivedEvent) {
+            string result = "";
+            switch(eventType) {
+                case createDigitalTwinEventType: case deleteDigitalTwinEventType: case updateDigitalTwinEventType:
+                    result = receivedEvent.Subject;
+                break;
+                case relationshipEventType:
+                    result = ((JObject)JsonConvert.DeserializeObject(receivedEvent.Data.ToString()))["$sourceId"].ToString();
+                break;
+            }
+            return result;
         }
     }
 }
